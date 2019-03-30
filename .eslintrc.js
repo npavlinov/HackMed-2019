0cc10c981b1a6a5a5393197dc2bfbdc9cc8cae9e
npavlinov <nplyubomirov1@sheffield.ac.uk>
2019-03-30 10:57:15 +0000

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: 'babel-eslint',
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+        ecmaeatures: {
+            experimentalObjectRestSpread: true,
+        },
+        sourceType: 'module',
+    },
+    rules: {
+        "comma-dangle": [1, "only-multiline"],
+        "no-multiple-empty-lines": "warn",
+        "no-var": "error",
+        "prefer-const": "error",
+        'no-console': 'off'
+    }
+}
